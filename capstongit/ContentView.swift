6a888ee510464a112973a1e3c9aa34b7c29bbd9d
parkhyunjun <wprzl479@naver.com>
2024-05-07 10:27:47 +0900

    Initial Commit

---
+++ capstongit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  capstongit
+//
+//  Created by 박현준 on 5/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
