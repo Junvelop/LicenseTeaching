e41c866d5274adfb46355e816d18cac004c6fbaa
parkhyunjun <wprzl479@naver.com>
2024-05-22 09:11:37 +0900

    refactoring 2

--- capstongit/ThirdPage.swift
+++ capstongit/ThirdPage.swift
@@ -5,4 +5,28 @@
 //  Created by 박현준 on 5/21/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ThirdPage: View {
+    @Binding var currentPage: Int
+    
+    var body: some View {
+        VStack {
+            Text("This is the Third Page")
+                .font(.largeTitle)
+            Spacer()
+            Button(action: {
+                self.currentPage = 1
+            }) {
+                Text("Go back to First Page")
+                    .padding(10)
+                    .font(.body)
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .cornerRadius(8)
+            }
+            .padding(.bottom, 20)
+        }
+        .navigationTitle("Third Page")
+    }
+}
